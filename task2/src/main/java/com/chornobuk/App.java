e5a6cc6f070acc05beedb858d3dcdeb687f4e12a
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-12-08 19:50:40 +0200

    untested version

--- task2/src/main/java/com/chornobuk/App.java
+++ task2/src/main/java/com/chornobuk/App.java
@@ -1,6 +1,13 @@
 package com.chornobuk;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
 public class App {
+
+    @Property(name = "firstname")
+    private static String name = "sfdsf";
+    
     public static void main(String[] args) {
         // Method[] methods = String.class.getMethods();
         // Type t = String.class;
@@ -9,6 +16,22 @@ public class App {
         // System.out.println(int.class.getTypeName());
         // System.out.println(Integer.class.getTypeName());
         // System.out.println(t.getTypeName());
+        try {
+            Field f = App.class.getDeclaredField("name");
+            System.out.println(Arrays.toString(f.getAnnotations()));
+            if(f.isAnnotationPresent(Property.class)) {
+                Property property = f.getAnnotation(Property.class);
+                System.out.println(property.format());
+                System.out.println(property.name());
+            }
+        } catch (NoSuchFieldException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
         // todo:
         // []1. create an annotation
         // []2. create class for reading properties file (read in map)
--- task2/src/main/java/com/chornobuk/ObjectLoader.java
+++ task2/src/main/java/com/chornobuk/ObjectLoader.java
@@ -1,35 +1,86 @@
 package com.chornobuk;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.nio.file.Path;
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 public class ObjectLoader {
     public static <T> T loadFromProperties(Class<T> cls, Path propertiesPath) {
+        try {
+            Properties properties = loadPropertiesFromFile(propertiesPath);
+            Map<String, Object> fieldValueMap = new HashMap<>();
+            Field[] fields = cls.getFields();
+            for (Field field : fields) {
+                String property = getPropertyValue(field, properties);
+                fieldValueMap.put(field.getName(), parseProperty(field, property));
+            }
+            System.out.println(fieldValueMap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
-    // searcher for property in map, parse from string and return value
-    private static Object parsePropertyFromMap(Field field, String property) {
-        // this method only parse value
+    // gets String value of the property, parses it and returns the result
+    private static Object parseProperty(Field field, String value) {
         if (field.getClass().equals(String.class)) {
-            return property;
+            return value;
         } else if (field.getClass().equals(Integer.class) || field.getClass().equals(int.class)) {
             try {
-                return Integer.parseInt(property);
+                return Integer.parseInt(value);
             } catch (NumberFormatException e) {
-                System.out.println(property + "cannot be cast to Integer");
+                System.out.println(value + "cannot be cast to Integer");
             }
         } else if (field.getClass().equals(Instant.class)) {
-            return null; // todo: parse instant
-            // todo: throw another exception if date cannot be parsed
+            try {
+                if (field.isAnnotationPresent(Property.class)) {
+                    Property property = field.getAnnotation(Property.class);
+                    if (!property.name().isEmpty()) {
+                        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(property.name())
+                                .withZone(ZoneId.systemDefault());
+                        Instant instant = Instant.from(dateTimeFormatter.parse(value));
+                        return instant;
+                    }
+                    return DateTimeFormatter.ISO_INSTANT.parse(value);
+                }
+
+            } catch (DateTimeParseException e) {
+                e.printStackTrace();
+            }
         } else {
             throw new IllegalArgumentException(field.getClass().getTypeName() + " is not supported");
         }
+        // todo: refactor it
+        return null;
     }
 
-    private Object getPropertyFromMap(Field field, Map<String, String> properties) {
-        return null;
+    // get property value by the name of the field or the name of Property
+    // annotation
+    // returns null if the property wasn't found
+    private static String getPropertyValue(Field field, Properties properties) {
+        String propertyName = field.getName();
+        if (field.isAnnotationPresent(Property.class)) {
+            Property property = field.getAnnotation(Property.class);
+            if (!property.name().isEmpty()) {
+                propertyName = property.name();
+            }
+        }
+        return properties.getProperty(propertyName);
+    }
+
+    private static Properties loadPropertiesFromFile(Path path) throws IOException {
+        Properties properties = new Properties();
+        properties.load(new FileInputStream(path.toFile()));
+        return properties;
     }
 }
