02f16d4b401e3bf18cb8bfe7165f0105c0ca0f32
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-12-11 15:08:10 +0200

    remove redundant fields

--- task2/src/test/java/com/chornobuk/testclasses/InfoIncorrectFormat.java
+++ task2/src/test/java/com/chornobuk/testclasses/InfoIncorrectFormat.java
@@ -5,15 +5,6 @@ import java.time.Instant;
 import com.chornobuk.Property;
 
 public class InfoIncorrectFormat {
-    private String value;
-
-    @Property(name = "lastname")
-    private String surname;
-
-    private int age;
-
-    private Integer height;
-
     @Property(format = "dddd.MMMM.yyyy tt:mm")
     private Instant instant;
 }
